01e84ad7e01950d0d2e6787c432e057da94c66e2
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2025-02-13 19:07:49 -0800

    Update ashHistory.py

--- scripts/artifacts/ashHistory.py
+++ scripts/artifacts/ashHistory.py
@@ -4,7 +4,7 @@ from scripts.artifact_report import ArtifactHtmlReport
 from scripts.lleapfuncs import logfunc, tsv, get_next_unused_name, get_user_name_from_home
 
 
-def get_python_history(files_found, report_folder, seeker, wrap_text):
+def get_ash_history(files_found, report_folder, seeker, wrap_text):
 
     for file_found in files_found:
         file_found = str(file_found)
@@ -20,9 +20,9 @@ def get_python_history(files_found, report_folder, seeker, wrap_text):
 
         usageentries = len(data_list)
         if usageentries > 0:
-            report = ArtifactHtmlReport(f'Python History of {user_name}')
+            report = ArtifactHtmlReport(f'Ash (Alphine Linux Command) History of {user_name}')
             #check for existing and get next name for report file, so report from another file does not get overwritten
-            report_path = os.path.join(report_folder, f'python_history_{user_name}.temphtml')
+            report_path = os.path.join(report_folder, f'ash_history_{user_name}.temphtml')
             report_path = get_next_unused_name(report_path)[:-9] # remove .temphtml
             report.start_artifact_report(report_folder, os.path.basename(report_path))
             report.add_script()
@@ -31,15 +31,16 @@ def get_python_history(files_found, report_folder, seeker, wrap_text):
             report.write_artifact_data_table(data_headers, data_list, file_found)
             report.end_artifact_report()
             
-            tsvname = f'python_history_{user_name}'
+            tsvname = f'ash_history_{user_name}'
             tsv(report_folder, data_headers, data_list, tsvname)
             
         else:
-            logfunc(f'No python history data for {user_name}available')
+            logfunc(f'No ASH history data for {user_name}available')
 
 __artifacts__ = {
-        "python_history": (
-                "Python History",
-                ('**/home/*/.python_history'),
-                get_bash_history)
+        "ash_history": (
+                "Ash History (Alphine Linux Command History)",
+                ('*/.ash_history'),
+                get_ash_history)
 }
+# ToDo, replace user name with the directory it was found in
